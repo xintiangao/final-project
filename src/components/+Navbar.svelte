9bb4b07a3a71f11abf5cbe2f7d211466478b580e
Cynthia Gao <gaoxintian08@gmail.com>
2023-07-03 14:34:12 +0800

    changed localhost 8000

--- src/components/+Navbar.svelte
+++ src/components/+Navbar.svelte
@@ -52,7 +52,14 @@ function goToSignInPage() {
 
     <div>
 		{#if $isAuthenticated}
-			<button on:click={logOut}>Sign Out</button>
+			<a href="/" on:click={logOut}>
+                <button class="btn btn-ghost btn-circle">
+                    <button class="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="23" y1="11" x2="17" y2="11"></line></svg>
+                
+                    </button>
+                </button>
+            </a>
 		{:else}
 			<a href="/auth" on:click={goToSignInPage}>
                 <button class="btn btn-ghost btn-circle">
