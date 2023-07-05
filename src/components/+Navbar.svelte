7a6c23f10d429092174011a5439859e04f15bb7f
Cynthia Gao <gaoxintian08@gmail.com>
2023-07-05 16:49:25 +0800

    resolved conflict

--- src/components/+Navbar.svelte
+++ src/components/+Navbar.svelte
@@ -26,7 +26,7 @@ function goToSignInPage() {
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
           <li><a href='/'>Homepage</a></li>
-          <li><a href='/'>Budget Calculator</a></li>
+          <li><a href='/'>Set Goals</a></li>
           <li><a href='/history'>Transaction History</a></li>
         </ul>
       </div>
